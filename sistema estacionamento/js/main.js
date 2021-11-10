e4e0b4a996ff5b3b1b2c8ffc64daf837b5500ce1
tygola <59832169+tygola@users.noreply.github.com>
2021-11-10 15:25:08 -0300

    Add files via upload

---
+++ sistema estacionamento/js/main.js
@@ -0,0 +1,71 @@
+document.getElementById('formulario').addEventListener('submit', cadastraVeiculo)
+
+function cadastraVeiculo(e) {
+    var modeloCarro = document.getElementById('modeloCarro').value;
+    var placaCarro = document.getElementById('placaCarro').value;
+    var time = new Date();
+
+    if(!modeloCarro && !placaCarro){
+        alert('Por favor, Digite o modelo e a placa do carro!');
+        return false;
+    }
+
+    carro = {
+        modelo: modeloCarro,
+        placa: placaCarro,
+        hora: time.getHours(),
+        minutos: time.getMinutes()
+    }
+
+    
+
+    if(localStorage.getItem('patio2') === null) {
+        var carros = [];
+        carros.push(carro);
+        localStorage.setItem('patio2', JSON.stringify(carros));
+    }else{
+        var carros = JSON.parse(localStorage.getItem('patio2'));
+        carros.push(carro);
+        localStorage.setItem('patio2', JSON.stringify(carros));
+    }
+
+    document.getElementById('formulario').reset();
+
+    mostraPatio();
+
+    e.preventDefault();
+}
+
+function apagarVeiculo(placa){
+    var carros = JSON.parse(localStorage.getItem('patio2'));
+
+    for(var i = 0; i < carros.length; i++){
+        if(carros[i].placa == placa){
+            carros.splice(i, 1);
+        }
+
+        localStorage.setItem('patio2', JSON.stringify(carros));
+    }
+
+    mostraPatio();
+}
+
+function mostraPatio(){
+    var carros = JSON.parse(localStorage.getItem('patio2'));
+    var carrosResultados = document.getElementById('resultados');
+
+    carrosResultados.innerHTML = '';
+
+    for(var i = 0; i < carros.length; i++){
+        var modelo = carros[i].modelo;
+        var placa = carros[i].placa;
+        var hora = carros[i].hora;
+        var minutos = carros[i].minutos;
+
+  carrosResultados.innerHTML += '<tr><td>' + modelo + 
+                                '</td><td>' + placa +
+                                '</td><td>' + hora + ' : ' + minutos +
+                                '</td><td><button class="btn btn-danger" onClick="apagarVeiculo(\'' + placa + '\')">Excluir</button></td>' +
+                                '</tr>'
+    }
+}
